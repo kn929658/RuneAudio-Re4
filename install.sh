f5664b3e61c5cde36ae4b4ec96e1816866b60f98
rern <rernrern@gmail.com>
2020-06-20 07:51:05 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,8 +4,6 @@ alias=rre4
 
 . /srv/http/bash/addons-functions.sh
 
-[[ ! -s /boot/cmdline.txt ]] && echo 'root=PARTUUID=6cf48b40-02 rw rootwait selinux=0 plymouth.enable=0 smsc95xx.turbo_mode=N dwc_otg.lpm_enable=0 elevator=noop fsck.repair=yes isolcpus=3 console=tty3 quiet loglevel=0 logo.nologo vt.global_cursor_default=0' > /boot/cmdline.txt
-
 installstart $@
 
 getinstallzip

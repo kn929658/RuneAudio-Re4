7b81bc20ff867b564f0fc82ec9333020cfcccedf
rern <rernrern@gmail.com>
2020-06-23 20:21:12 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,6 +6,13 @@ alias=rre4
 
 installstart $@
 
+hwcode=$( grep Revision /proc/cpuinfo )
+if [[ ${hwcode: -3:2} =~ ^(09|0c)$ ]]; then
+	sed -i -e '$ a\dtoverlay=vc4-kms-v3d
+' -e '/dtparam=audio=on/ d
+' /boot/config.txt
+fi
+
 getinstallzip
 
 installfinish $@

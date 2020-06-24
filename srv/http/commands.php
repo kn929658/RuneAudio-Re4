bee54472d686b433c73b06cac175a5ada96efde8
rern <rernrern@gmail.com>
2020-06-24 09:52:31 +0700

    Update commands.php

--- srv/http/commands.php
+++ srv/http/commands.php
@@ -176,7 +176,7 @@ if ( isset( $_POST[ 'backuprestore' ] ) ) {
 			$profile = preg_replace( '/\**\s*/', '', $profile );
 			$data.= $profile."\n";
 			$data.= "------------------------------\n";
-			$data.= shell_exec( "cat /etc/netctl/Home2GHz | sed 's/Key=.*/Key=\"*********\"/'" );
+			$data.= shell_exec( "cat /etc/netctl/Home2GHz | sed -e '/^#.*/ d' -e 's/Key=.*/Key=\"*********\"/'" );
 		}
 	} else {
 		$data = '(none)';

1c8225f82664c1e19f50ec8b05d2ffa2e867418b
rern <rernrern@gmail.com>
2020-06-24 14:41:09 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,19 +8,6 @@ installstart $@
 
 sed -i '/dtoverlay=vc4-kms-v3d/ d' /boot/config.txt
 
-hwcode=$( grep Revision /proc/cpuinfo | tail -c 4 | cut -c1-2 )
-file=/srv/http/data/system/audio-aplayname
-file1=/srv/http/data/system/audio-output
-if grep -q 'bcm2835 ALSA_1' $file; then
-	if [[ $hwcode =~ ^(09|0c)$ ]]; then
-		echo 'bcm2835 HDMI 1' > $file
-		echo 'On-board - HDMI' > $file1
-	else
-		echo 'bcm2835 Headphones' > $file
-		echo 'On-board - Headphone' > $file1
-	fi
-fi
-
 getinstallzip
 
 installfinish $@

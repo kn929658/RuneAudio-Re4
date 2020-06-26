c6e37c2a47f6f1b105aff5f90d264cd15162491d
rern <rernrern@gmail.com>
2020-06-26 17:36:12 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,16 +6,28 @@ alias=rre4
 
 installstart $@
 
-echo '
+if grep -q rewrite /etc/nginx/nginx.conf; then
+	sed -i -e '/rewrite/ d
+' -e '/cache busting/ {n;d}
+' -e '/try_files/ i\
+		location ~* (.+)\\.(?:\\d\\d\\d\\d\\d\\d\\d\\d\\d\\d)\\.(css|js|jpg|jpeg|gif|png|svg|ttf|woff)$ {
+' /etc/nginx/nginx.conf
+fi
+
+if ! grep -q [RR] /etc/pacman.conf; then
+	echo '
 [RR]
 SigLevel = Optional TrustAll
 Server = https://rern.github.io/$arch
 ' >> /etc/pacman.conf
+fi
 
 sed -i '/dtoverlay=vc4-kms-v3d/ d' /boot/config.txt
 
 getinstallzip
 
 installfinish $@
+
+systemctl restart nginx
 
 restartlocalbrowser

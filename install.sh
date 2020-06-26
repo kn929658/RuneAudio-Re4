5254f4c7398d198019d005e8a694abc7e44d9e60
rern <rernrern@gmail.com>
2020-06-26 07:26:04 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,13 +6,11 @@ alias=rre4
 
 installstart $@
 
-hwcode=$( grep Revision /proc/cpuinfo )
-[[ ${hwcode: -4:1} == 0 ]] && repo=armv6h || repo=armv7h
-echo "
+echo '
 [RR]
 SigLevel = Optional TrustAll
-Server = https://rern.github.io/$repo
-" >> /etc/pacman.conf
+Server = https://rern.github.io/$arch
+' >> /etc/pacman.conf
 
 sed -i '/dtoverlay=vc4-kms-v3d/ d' /boot/config.txt
 

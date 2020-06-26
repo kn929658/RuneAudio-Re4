00f59a582fff408709d0e75065747f321a6d810d
rern <rernrern@gmail.com>
2020-06-26 17:08:57 +0700

    Update main.js

--- srv/http/assets/js/main.js
+++ srv/http/assets/js/main.js
@@ -37,7 +37,7 @@ var picaOption = { // pica.js
 //	, quality          : 3    // 0...3 Default = 3 (Lanczos win=3)
 //	, alpha            : true // Default = false (black crop background)
 };
-var hash = Date.now();
+var hash = Math.ceil( Date.now() / 1000 );
 var coverrune = '/assets/img/cover.'+ hash +'.svg';
 var vustop = '/assets/img/vustop.'+ hash +'.gif';
 if ( G.localhost ) {
@@ -1466,7 +1466,7 @@ $( '#mode-coverart' ).click( function() { // fix - 'tap' also fire .coverart cli
 			  icon    : 'coverart'
 			, title   : 'Create CoverArt Thumbnails'
 			, message : 'Find coverarts and create thumbnails.'
-					   + ( albumcount > 100 ? '<br>( ±'+ Math.ceil( albumcount / 100 ) +' minutes for '+ albumcount +' albums)<br>&nbsp;' : '' )
+					   + ( albumcount > 60 ? '<br>( ±'+ Math.ceil( albumcount / 60 ) +' minutes for '+ albumcount +' albums)<br>&nbsp;' : '' )
 			, checkbox : {
 				  'Update Library database'         : 1
 				, 'Replace existings'               : 1

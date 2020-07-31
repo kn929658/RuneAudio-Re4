7077bba453125219b3b56b55670f455c3ca04a5a
rern <rernrern@gmail.com>
2020-07-31 14:22:58 +0700

    Update cmd-coverart.sh

--- srv/http/bash/cmd-coverart.sh
+++ srv/http/bash/cmd-coverart.sh
@@ -2,7 +2,7 @@
 
 path="/mnt/MPD/$1"
 
-#1 - get coverfile in directory
+### 1 - get coverfile in directory ##################################
 [[ -d $path ]] && dir=$path || dir=$( dirname "$path" )
 for name in cover folder front thumb album; do
 	for ext in jpg png gif; do
@@ -14,7 +14,7 @@ for name in cover folder front thumb album; do
 	coverfile=
 done
 
-#2 - get embedded in file
+### 2 - get embedded in file ########################################
 if [[ -n $coverfile ]]; then
 	# convert % > ^ | replace " > %20 | convert ^ > % for img src="/encoded/url"
 	coverfile=$( sed 's/%/\^/g; s/"/%22/g; s/\^/%25/g' <<< $coverfile )
@@ -33,7 +33,7 @@ else
 	rm -f $tmpfile
 #	ffmpeg -i "$file" $tmpfile &> /dev/null
 	kid3-cli -c "select \"$file\"" -c "get picture:$tmpfile" &> /dev/null # suppress '1 space' stdout
-	#3 - fetch online
+### 3 - fetch online ################################################
 	if [[ ! -e $tmpfile ]]; then
 		/srv/http/bash/cmd-coverartfetch.sh "$2" &> /dev/null &
 		exit
@@ -43,7 +43,7 @@ else
 	ext=jpg
 fi
 
-if [[ $2 != thumbnail || $ext == gif ]]; then # $# == 2 : thumbnail
+if [[ $2 != thumbnail || $ext == gif ]]; then
 	echo "$coverfile.$( date +%s ).$ext"
 else # resize
 	convert "$coverfile.$ext" -thumbnail 200x200 -unsharp 0x.5 inline:JPG:-

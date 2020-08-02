9af9facb78fae6cba39dd1eb6144c9966205f931
rern <rernrern@gmail.com>
2020-08-02 15:31:49 +0000

    Add files via upload

--- srv/http/cmd.php
+++ srv/http/cmd.php
@@ -193,7 +193,7 @@ case 'imagefile':
 		}
 	} else {
 		unlink( $imagefile );
-		exec( '/srv/http/bash/cmd.sh "countcoverart\n-1"' );
+		exec( $sudo.'/srv/http/bash/cmd.sh countcoverart' );
 	}
 	if ( isset( $_POST[ 'base64' ] ) ) {
 		if ( !isset( $_FILES[ 'file' ] ) ) {
